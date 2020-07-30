9c4e2475a783659553e00bd125b98cac0b5402e7
Sabita Neupane <nsabita.sasa@gmail.com>
2020-07-30 08:59:45 +0545

    Add JS Math and Date object

--- javascript/asset/js/time.js
+++ javascript/asset/js/time.js
@@ -1,3 +1,11 @@
+// Write a JavaScript program to display the current day and time in the following format.
+// Current time is : 10 PM : 30 : 38
+
+// var today = new Date();
+// var hour = today.getHours();
+// var minute = today.getMinutes();
+// var second = today.getSeconds();
+
 var hour = 00;
 var minute = 00;
 var second = 00;
