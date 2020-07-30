50a0ee3fa9b832e62c0023addfc2831153dcdd36
Sabita Neupane <nsabita.sasa@gmail.com>
2020-07-30 09:02:32 +0545

    Add formula

--- javascript/asset/js/area-of-traingle.js
+++ javascript/asset/js/area-of-traingle.js
@@ -1,9 +1,10 @@
 // Write a JavaScript function to find the area of a triangle where lengths of the three of its sides are 5, 6, 7.
+// Area = √( s(s-a)(s-b)(s-c)) where s=(a+b+c)/2
 
-var side1 = 5; 
-var side2 = 6; 
-var side3 = 7; 
-var s = (side1 + side2 + side3)/2;
-var area =  Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
+var side1 = 5;
+var side2 = 6;
+var side3 = 7;
+var s = (side1 + side2 + side3) / 2;
+var area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3)));
 console.log(area);
 document.write(area);
