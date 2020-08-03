75eb3ff090a463a212552c21292ac4c0a54b0b6f
Sabita Neupane <nsabita.sasa@gmail.com>
2020-08-03 17:27:05 +0545

    calculator

--- javascript/asset/js/calculator.js
+++ javascript/asset/js/calculator.js
@@ -1,5 +1,10 @@
-var a = 20, b = 5;
+// var a = 20, b = 5;
+
+
 function calculate(operator) {
+  var a = Number(document.getElementById("num1").value);
+  var b = Number(document.getElementById("num2").value);
+
   var cal = document.getElementById("calculator-value");
   if (operator === "+") {
     cal.innerHTML = a + b;
